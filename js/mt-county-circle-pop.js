30798e3d04af4625ad112adc3c41186187a4e6f0
eidietrich <eric.dietrich@gmail.com>
2016-10-27 21:48:29 -0600

    More refactor

--- js/mt-county-circle-pop.js
+++ js/mt-county-circle-pop.js
@@ -1,74 +1,86 @@
-var width = 500, height = 400;
+var MapGen = function (props, globals){
+  this.data = props.data;
+  this.element = props.element;
+  this.width = props.width;
+  this.height = props.height;
 
-    var svg = d3.select('#map')
+  console.log(this.data.counties);
+  console.log(this.data.votes);
+
+  this.draw()
+}
+MapGen.prototype.draw = function() {
+
+  this.svg = d3.select(this.element)
       .append('svg')
-      .attr("width", width)
-      .attr("height", height)
+      .attr("width", this.width)
+      .attr("height", this.height)
 
+  // Initialize projection
+  var projection = d3.geoMercator()
+    .fitExtent([[10,10],[this.width, this.height]], this.data.counties);
+  var path = d3.geoPath()
+    .projection(projection);
 
-
-    var scale = d3.scaleLinear()
-      .range([0,1600]);
-    var voterScale = function(number){
-      // returns radius for proportional area, I think
-      return Math.sqrt(scale(number));
-    }
-
-    var colorScale = d3.scaleLinear()
-      .domain([0,0.35,0.5,0.65,1]) // Fraction vote for Obama in 2012
-      .range(["#b2182b","#d6604d","#756bb1","#4393c3","#2166ac"])
-      .interpolate(d3.interpolateLab);
-
-    var format = d3.format(".0%");
-
-    d3.json('./../data/mt-counties-simplified.geojson', function(data){
-
-      // console.log(data);
-
-      var projection = d3.geoMercator()
-        .fitExtent([[10,10],[width, height]], data);
-
-      var path = d3.geoPath()
-        .projection(projection);
-
-      svg.append("g")
-        .selectAll("path")
-        .data(data.features)
-        .enter().append("path")
-        .attr("d", path)
-        .attr("class", "county");
-
-        d3.json('./../data/2012-election-points.geojson', function(points) {
-          scale.domain(d3.extent(points.features, function(d){
-            return d.properties['reg_voters'];
-          }));
-
-          circles = svg.append("g")
-            .selectAll('.circles')
-            .data(points.features)
-            .enter();
-
-          circles.append("circle")
-            .attr("transform", function(d) {return "translate(" + projection(d.geometry.coordinates) + ")"; })
-            .attr("r", function(d) {
-              return voterScale(d.properties['reg_voters']) } )
-            .attr("fill", function(d){
-              testerVotes = d.properties['tester_votes'];
-              votesCast = d.properties['votes_cast'];
-              return colorScale(testerVotes / votesCast);
-            })
-            .attr("class", "circle");
-
-          circles.append("text")
-            .attr("transform", function(d) {return "translate(" + projection(d.geometry.coordinates) + ")"; })
-            .text(function(d){
-              testerVotes = d.properties['tester_votes'];
-              votesCast = d.properties['votes_cast'];
-              return format(testerVotes / votesCast);
-            })
-            .attr("class","label");
-
-
-        })
-
+  // Join data on NAME
+  var that = this;
+  this.data.counties.features.forEach(function(county){
+    that.data.votes.forEach(function(d){
+      if (d['NAME'] == county.properties['NAME']){
+        county.properties.voters = d["reg_voters"];
+        county.properties.votesCast = d["votes_cast"];
+        county.properties.votesForA = d["tester_votes"];
+      };
     });
+  });
+
+  // Create center_coords
+  this.data.counties.features.forEach(function(county){
+    county.properties.centerCoords = [+county.properties["center_lon"], +county.properties["center_lat"]];
+  });
+
+  console.log(this.data.counties);
+
+
+
+  // Draw counties
+  this.svg.append("g")
+    .selectAll("path")
+    .data(this.data.counties.features)
+    .enter().append("path")
+    .attr("d", path)
+    .attr("class", "county");
+
+  // Add circles, scaled by voter numbers, colored by turnout
+  globals.scale.domain(d3.extent(this.data.counties.features, function(d){
+    return d.properties.voters;
+  }));
+
+  var that = this;
+  circles = this.svg.append("g")
+    .selectAll('.circles')
+    .data(this.data.counties.features)
+    .enter();
+
+  circles.append("circle")
+    .attr("transform", function(d) {return "translate(" + projection(d.properties.centerCoords) + ")"; })
+    .attr("r", function(d) {
+      return globals.voterScale(d.properties.voters) } )
+    .attr("fill", function(d){
+      votesForA = d.properties.votesForA;
+      votesCast = d.properties.votesCast;
+      return globals.colorScale(votesForA / votesCast);
+    })
+    .attr("class", "circle");
+
+  circles.append("text")
+    .attr("transform", function(d) {return "translate(" + projection(d.properties.centerCoords) + ")"; })
+    .text(function(d){
+      votesForA = d.properties.votesForA;
+      votesCast = d.properties.votesCast;
+      return globals.format(votesForA / votesCast);
+    })
+    .attr("class","label");
+  }
+
+MapGen.prototype
