a6d82be146e177db14393939d0735fec26ac7d64
eidietrich <eric.dietrich@gmail.com>
2016-11-05 10:35:04 -0600

    got TableByCounty.js working

--- js/TableByCounty.js
+++ js/TableByCounty.js
@@ -1,38 +1,28 @@
 // Table by county
 
-// Renders county level data for races
-// this.data = geoJson with prop.candidates array
-
-/*
-Columns:
-- County name
-- Precincts counted
-- R candidate name
-- D candidate name
-- R vote percent
-- D vote percent
-- Other vote names / percent?
-
-Other info to communicate: is one candidate the incumbent?
-Libertarian candidates?
-*/
-
-var candidates = [
-  {'name': 'ZINKE', 'party': 'R', 'incumbent': 'yes'},
-  {'name': 'JUNEAU', 'party': 'R', 'incumbent': 'no'},
-  {'name': 'BRECKENRIDGE', 'party': 'L', 'incumbent': 'no'},
-];
+// Renders a by-county table for given race
 
 var TableByCounty = function (props){
-  this.data = props.data;
+
   this.element = props.element;
   // this.height = props.height;
+
+  // data variables
+  this.data = props.data; // input data, as geoJson
+  // container for data passed to draw function
+  this.tableData = {'columns': [], 'rows': []};
+  // supplemental info (e.g. party, incumbency)
+  this.extraCandidateInfo = props.extraCandidateInfo;
+  this.candidateKeys = []; // array of candidates, by last name
+  this.candidates = []; // array of candidates in race, by info objects
 
   console.log(this.data);
   this.shapeData();
   this.draw();
 }
 TableByCounty.prototype.shapeData = function() {
+  var that = this;
+
   function getVotesFor(objectArray, lastName){
     var votes;
     objectArray.forEach(function(d){
@@ -57,38 +47,72 @@ TableByCounty.prototype.shapeData = function() {
     return total;
   }
   function calcPercent(numer, denom){
-    if (denom === 0){ return 'n/a'; }
-    return numer / denom;
+    if (denom === 0){ return 'n/a %'; }
+    return d3.format(".0%")(numer / denom);
+  }
+  function getCandidates(){
+    // List candidates in race, add supplemental data
+    // TODO: Add in sorting by party? R, D, L, other
+
+    that.data.features.forEach(function(feature){
+      feature.properties.candidates.forEach(function(d){
+        var key = d.candidate_last;
+        if (that.candidateKeys.indexOf(key) < 0) {
+          that.candidateKeys.push(key);
+          var candidateInfo = {
+            'fullName': d.candidate_first + " " + d.candidate_last,
+            'lastName': key,
+            'party': null,
+            'incumbent': 'no'
+          };
+          if (key in that.extraCandidateInfo) {
+            candidateInfo.party = that.extraCandidateInfo[key].party;
+            candidateInfo.incumbent = that.extraCandidateInfo[key].incumbent;
+          }
+          that.candidates.push(candidateInfo);
+        }
+      });
+    });
   }
 
-  var that = this;
+  getCandidates();
+  // console.log(this.candidateKeys);
+  // console.log(this.candidates);
 
   // sort county data by number of precincts, descending
   this.data.features.sort(function(a,b){
     return b.properties.precincts - a.properties.precincts;
   });
 
-  this.tableData = {};
-  this.tableData.rows = [];
   this.data.features.forEach(function(feature){
     var properties = feature.properties;
-    var candidates = properties.candidates;
-    // console.log(candidates)
-    // console.log('result', getVotesFor(candidates, 'ZINKE'))
 
-    var row = {
-      'County': feature.properties.NAME,
-      'Precincts Counted': String(properties.precinctsReporting) + '/' + String(properties.precincts),
-      'Zinke votes': getVotesFor(candidates, 'ZINKE'),
-      'Zinke percent': getPercentFor(candidates, 'ZINKE'),
-      'Juneau votes': getVotesFor(candidates, 'JUNEAU'),
-      'Juneau percent': getPercentFor(candidates, 'JUNEAU')
+    var row = {};
+    // values and display separated here in the hope of making
+    // value-specific formatting easier down the line
+    row.values = {
+      'precincts': String(properties.precincts),
+      'precinctsReporting': String(properties.precinctsReporting),
+      'totalVotes': totalOf(properties.candidates, 'votes'),
     };
+    row.display = {
+      'County': feature.properties.NAME,
+      'Precincts<br />Reporting':  row.values.precinctsReporting + '/' + row.values.precincts,
+    };
+
+    // Add values for each candidate
+    that.candidateKeys.forEach(function(key){
+      var votes = getVotesFor(properties.candidates, key);
+      var percent = getPercentFor(properties.candidates, key);
+
+      row.values[key + '_votes'] = votes;
+      row.values[key + '_percent'] = percent;
+      row.display[key] = votes + " votes<br/>(" + percent + ")";
+    })
+
     that.tableData.rows.push(row);
   });
-  this.tableData.columns = Object.keys(this.tableData.rows[0]);
-
-
+  this.tableData.columns = Object.keys(this.tableData.rows[0].display);
 
 }
 TableByCounty.prototype.draw = function() {
@@ -98,7 +122,7 @@ TableByCounty.prototype.draw = function() {
 
   var table = d3.select(this.element)
     .append('table')
-    .attr("classs", "table");
+    .attr("class", "table");
   var thead = table.append('thead'),
     tbody = table.append('tbody');
   // headers
@@ -106,23 +130,17 @@ TableByCounty.prototype.draw = function() {
     .selectAll('th')
     .data(this.tableData.columns).enter()
     .append('th')
-    .text(function(d){ return d; });
+    .html(function(d){ return d; });
   // rows
   var rows = tbody.selectAll('tr')
     .data(this.tableData.rows).enter()
     .append('tr')
+  // cells
+  // TODO: Figure out how to add selective formatting here
   var cells = rows.selectAll('td')
     .data(function(row, i){
-      return that.tableData.columns.map(function(key){ return row[key]; });
-    })
-    .enter()
+      return that.tableData.columns.map(function(key){ return row.display[key]; });
+    }).enter()
     .append("td")
-    .text(function(d){ return d; })
+    .html(function(d){ return d; })
 }
-
-/* Table data object, ideal form
-- columns
-- rows
-- - {columnName: value } - [countyName ]
-
-*/
